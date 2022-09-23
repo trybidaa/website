94a29f504e483758ac3a5669ecdbae6a93e3bfec
Adetayo Akinsanya <akinsanyaadetayo@gmail.com>
2022-09-23 03:32:47 +0100

    Home page done"

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -49,11 +49,12 @@ const Navigation = () => {
             </div>
             <div className="-mr-2 flex md:hidden">
               <button
-                type="button"
-                onClick={() => setIsOpen(!isOpen)}
-                className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
+                className="block h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
               >
                 <span className="sr-only">Open main menu</span>
 
