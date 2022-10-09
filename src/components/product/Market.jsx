0b189c7d35c2b84ced04400bc443b2b12b202127
Adetayo Akinsanya <akinsanyaadetayo@gmail.com>
2022-10-09 23:01:35 +0100

    Fix

--- src/components/product/Market.jsx
+++ src/components/product/Market.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Container from '../container/Container';
-import Chart from '../../assets/images/chart.png';
+import Chart from '../../assets/images/bidhaa-webapp.svg';
+
 import { Button } from '../navigation/Navigation';
 
 const Market = () => {
   return (
     <Container reverse={true}>
-      <div className="font-light md:px-20 py-10 order-2 md:order-1">
+      <div
+        data-aos="fade-right"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+        data-aos-offset="200"
+        className="font-light md:px-20 py-10 order-2 md:order-1"
+      >
         <h2 className="text-4xl font-semibold">Bidhaa Market</h2>
         <div className="py-8">
           <p className="py-4">
@@ -20,12 +27,18 @@ const Market = () => {
             interest or expertise.
           </p>
 
-          <Button bgColor="bg-bidhaa-blue">
+          <Button bgColor="bg-bidhaa-purple">
             <a href="#wait-list">Join Waitlist</a>
           </Button>
         </div>
       </div>
-      <div className="order-2 md:order-1">
+      <div
+        data-aos="fade-left"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+        data-aos-offset="300"
+        className="order-2 md:order-1"
+      >
         <img src={Chart} alt="desk" />
       </div>
     </Container>
--- src/components/product/ProductX.jsx
+++ src/components/product/ProductX.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import Container from '../container/Container';
-import Desk from '../../assets/images/desk.png';
+import Desk from '../../assets/images/bidhaa-webapp.svg';
 
 const ProductX = () => {
   return (
     <Container>
-      <div>
+      <div
+        data-aos="fade-right"
+        // data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+        // data-aos-offset="200"
+      >
         <img src={Desk} alt="desk" />
       </div>
-      <div className="font-light md:px-20 py-10">
+      <div
+        data-aos="fade-left"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+        data-aos-offset="300"
+        className="font-light md:px-20 py-10"
+      >
         <h2 className="text-4xl font-semibold">ProductX</h2>
         <div className="py-8">
           <p className="py-4">
